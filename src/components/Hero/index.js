9b0b90ea7e01dd3e105054df5084826fdeb16bae
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-01-18 11:39:29 +0100

    Photo added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,16 @@
+import { Image } from "./Image/styled";
 import { Wrapper } from "./styled";
 import ThemeChanger from "./ThemeChanger";
+import myPhoto from "./images/Hania-cv-sepia.png"
 const Hero = () => (
-  <Wrapper><ThemeChanger
-  text={"dark mode off"}
-  /></Wrapper>
-  );
-  
-  export default Hero;
+  <Wrapper>
+    <ThemeChanger
+      text={"dark mode off"}
+    />
+    <Image
+      src={myPhoto}
+    />
+  </Wrapper>
+);
+
+export default Hero;
