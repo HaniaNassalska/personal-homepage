f643de9d629543a3405601713ff2141c62fd9bdf
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-01-19 09:10:23 +0100

    new theme color added

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,8 @@ const theme = {
     mineShaft: "#252525",
     slateGray: "#6E7E91",
     mercury: "#E5E5E5",
-    dodgerBlue: "#2188FF"
+    dodgerBlue: "#2188FF",
+    cornflowerBlue: "#6195ED"
   },
 
   breakpoint: {
