23b2296d9c83126e7121c3bb3c68eb4e57bfa976
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-02-08 10:37:30 +0100

    view of projects name changed

--- src/components/features/homepage/Portfolio/success/index.js
+++ src/components/features/homepage/Portfolio/success/index.js
@@ -1,18 +1,20 @@
 import { useSelector } from "react-redux";
 import { selectProjects } from "../../homepageSlice";
 import {
+  Wrapper,
   ContentContainer,
   GitHubIco,
   Header,
-  Paragraph,
+  Paragraph
+} from "../styled";
+import {
   PortfolioTile,
   TileHeader,
   TileText,
   TileTextLink,
-  Wrapper,
   TileLink,
   LinkWrapper
-} from "../styled";
+} from "./styled"
 
 const SuccessView = () => {
   const projects = useSelector(selectProjects)
@@ -24,21 +26,36 @@ const SuccessView = () => {
         <Header>Portfolio</Header>
         <Paragraph>My recent projects</Paragraph>
       </ContentContainer>
-      {projects.map(project => (
-        <PortfolioTile  key={project.id}>
-          <TileHeader>{(project.name.replaceAll( "-", " ").replaceAll("_", " "))}</TileHeader>
-          <TileText >  {project.description}</TileText>
-          <LinkWrapper>
-          <TileTextLink> Demo: </TileTextLink><TileLink href={project.homepage}>{project.homepage}</TileLink>
-         
-
-          <TileTextLink> Code: </TileTextLink> <TileLink href={project.html_url}> {project.html_url}</TileLink>
-          </LinkWrapper>
-
-        </PortfolioTile>
-      ))
-
-
+      {projects.map(project => {
+        const projectNameWithoutDash =
+          (project.name.replaceAll("-", " ")
+          ).replaceAll("_", " ");
+        const nameOfProjects =
+          projectNameWithoutDash.charAt(0).toUpperCase()
+          + projectNameWithoutDash.slice(1);
+        return (
+          <PortfolioTile key={project.id}>
+            <TileHeader>
+              {nameOfProjects}
+            </TileHeader>
+            <TileText >
+              {project.description}
+            </TileText>
+            <LinkWrapper>
+              <TileTextLink> Demo: </TileTextLink>
+              <TileLink
+                href={project.homepage}>
+                {project.homepage}
+              </TileLink>
+              <TileTextLink> Code: </TileTextLink>
+              <TileLink
+                href={project.html_url}>
+                {project.html_url}
+              </TileLink>
+            </LinkWrapper>
+          </PortfolioTile>
+        )
+      })
       }
     </Wrapper>
   )
