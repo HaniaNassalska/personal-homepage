9bb38242eae3da05476c5b5ae50fd17bfd34a68e
Hanna Nassalska <hanna.nassalska@gmail.com>
2023-03-09 13:07:44 +0100

    code formatted

--- src/components/core/App/GlobalStyle.js
+++ src/components/core/App/GlobalStyle.js
@@ -10,10 +10,9 @@ html {
   }
 
   body {
-    background-color: ${({theme})=> theme.colors.background};
+    background-color: ${({ theme }) => theme.colors.background};
     font-family: "Inter", sans-serif;
     word-break: break-word;
     transition: 0.3s;
   }
   `;
-  
